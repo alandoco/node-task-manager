59160456a11341cd1b9be1a99fffca07d5111693
Alan Rochford <alan.rochford@eventbase.com>
2020-04-09 16:40:16 -0700

    Lecture 110 initial authentication added

---
+++ src/middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+    try{
+        //Getting token from header and removing Bearer string
+        const token = req.header('Authorization').replace('Bearer ','')
+        const decoded = jwt.verify(token, 'thisismynewcourse')
+        //This finds a user using the id from the decoded token
+        //And also searches that users token array(tokens.token)
+        //to check if it exists
+        console.log(token)
+        console.log(decoded)
+        const user = await User.findOne({
+            _id: decoded._id, 
+            'tokens.token': token
+        })
+
+        console.log(user)
+
+        if(!user){
+            //This triggers the catch
+            throw new Error()
+        }
+
+        //This passes the user document into the request so it
+        //can be used in the route handler
+        //Avoids the route handler having to re-fetch the user
+        req.user = user
+        next()
+    } catch(e){
+        res.status(401).send({error: 'Please authenticate'})
+    }
+}
+
+module.exports = auth
